8ffa60d91f530b6af4afc9fa7fc175b60205bf46
Stephen Young <stephenyoung@Stephens-MacBook-Pro.local>
2022-08-27 22:49:31 -0400

    First Commit

---
+++ src/classes/Step.ts
@@ -0,0 +1,15 @@
+export type StepType = { read?: number, write?: number, util?: number };
+
+class Step {
+    value: StepType;
+
+    constructor(step: StepType) {
+        this.value = {
+            read: step.read,
+            write: step.write,
+            util: step.util,
+        }
+    }
+}
+
+export default Step;
